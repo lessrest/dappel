b86ae5492697ed63b4a1c5f6c61e227c27531aef
Mikael Brockman <mikael@brockman.se>
2018-01-09 10:01:09 +0200

    very amazing dapp-dwim

--- dapp.el
+++ dapp.el
@@ -31,18 +31,21 @@
 (defvar *ethereum-accounts* nil)
 
 (defun dapp-token-address (gem)
-  (cdr gem))
+  (let ((x (assoc gem *erc20-tokens*)))
+    (if x
+        (cdr x)
+      (error "Unknown ERC20 token `%s'" gem))))
 
 (defun seth-setup ()
   (setenv "ETH_RPC_URL" *ethereum-rpc-url*))
 
-(defun seth-accounts ()
-  (or *ethereum-accounts*
-      (mapcar #'split-string (split-string (seth "accounts") "\n" t))))
-
-(defun eth-pick-account ()
-  (interactive)
-  (completing-read "Account address: " (seth-accounts)))
+;;;; these commands have their conses backwards and are maybe not useful
+;; (defun seth-accounts ()
+;;   (or *ethereum-accounts*
+;;       (mapcar #'split-string (split-string (seth "accounts") "\n" t))))
+;; (defun eth-pick-account ()
+;;   (interactive)
+;;   (completing-read "Account address: " (seth-accounts)))
 
 (defun seth (command &rest args)
   (seth-setup)
@@ -77,16 +80,16 @@
 
 (defun dapp-insert-timestamp ()
   (interactive)
-  (insert (format-time-string "%Y-%m-%d %H:%M:%S")))
+  (insert (format-time-string "%Y-%m-%d %H:%M:%S %z")))
 
-(defun token-ls ()
-  (interactive)
+(defun dapp-scan-accounts-for-tokens (accounts)
   (insert ";; Token balances at ")
   (dapp-insert-timestamp)
+  (insert ":")
   (newline)
-  (dolist (x (seth-accounts))
-    (let ((address (car x)))
-      (insert address "   " (seth "nonce" address) "  " (cdr x))
+  (dolist (x accounts)
+    (let ((address (cdr x)))
+      (insert address "   " (seth "nonce" address) "  " (car x))
       (newline)
       (dolist (token *erc20-tokens*)
         (let ((balance (format-wei (seth-token-balance address (car token)))))
@@ -94,6 +97,10 @@
             (insert "  " (symbol-name (car token)) "    " balance)
             (newline))))))
   (newline))
+
+;; (defun token-ls ()
+;;   (interactive)
+;;   (dapp-scan-accounts-for-tokens (seth-accounts)))
 
 (defun dapp-grok-token-balance-line ()
   (save-excursion
@@ -104,19 +111,13 @@
           (re-search-backward "^\\(0x[0-9a-fA-F]\\{40\\}\\) ")
           (list (match-string 1) token amount)))))
 
-(defun dapp-make-transfer-tx ()
-  (interactive)
-  (seq-let (src gem max-wad) (dapp-grok-token-balance-line)
-    (let* ((dst (read-string "Receiving address: "))
-           (wad (read-string
-                 (concat "Amount of " (symbol-name gem) ": ") max-wad))
-           (wei (dapp-ether-to-wei wad)))
-      (goto-char (point-max))
-      (insert
-       (if (eq gem 'ETH)
-           (seth "mktx" "-V" wei "-F" src dst)
-         (seth "mktx" "-V" wei "-F" src (dapp-token-address gem)
-               "transfer(address,uint256)" dst wei))))))
+(defun dapp-transfer (src dst gem wad)
+  (let ((wei (dapp-ether-to-wei wad)))
+    (format "%S"
+     (if (eq gem 'ETH)
+         (seth "send" "--async" "-V" wei "-F" src dst)
+       (seth "send" "--async" "-F" src (gem-token-address gem)
+             "transfer(address,uint256)" dst wei)))))
 
 (defvar dapp-font-lock-defaults
   `((
@@ -131,3 +132,47 @@
   (setq font-lock-defaults dapp-font-lock-defaults)
   (setq comment-start ";")
   (setq comment-end ""))
+
+(defvar dapp-re-address "0x[0-9a-fA-F]\\{40\\}")
+
+(defun dapp-dwim ()
+  (interactive)
+  (let ((memory *ethereum-accounts*))
+    (save-excursion
+      (goto-char (point-min))
+      (while (< (point) (point-max))
+        (when (looking-at
+               (concat "^> remember \\(.*?\\) is \\(" dapp-re-address "\\)$"))
+          (let ((new (cons (match-string 1) (match-string 2))))
+            (setf memory (cons new memory))
+            (forward-line)
+            (unless (looking-at "^;; Token balances")
+              (dapp-scan-accounts-for-tokens (list new))
+              (forward-line -1))))
+        (when (looking-at
+               (concat "^> transfer \\([0-9.]+\\) \\([A-Z]+\\) from "
+                       "\\(.+?\\) to \\(.+?\\)$"))
+          (forward-line)
+          (let ((wad (match-string 1))
+                (gem (intern (match-string 2)))
+                (src-name (match-string 3))
+                (dst-name (match-string 4)))
+            (let ((src-cons (assoc-string src-name memory))
+                  (dst-cons (assoc-string dst-name memory)))
+              (unless src-cons (error "Unknown account `%s'" src-name))
+              (unless dst-cons (error "Unknown account `%s'" dst-name))
+              (if (looking-at "^;; Tx hash: \\(.*?\\)$")
+                  (let ((tx-hash (match-string 1)))
+                    (forward-line 1)
+                    (unless (looking-at "^;; Included in block ")
+                      (let ((number (seth "receipt" tx-hash "blockNumber")))
+                        (insert ";; Included in block " number ".\n")
+                        (dapp-scan-accounts-for-tokens
+                         (list src-cons dst-cons))
+                        (forward-line -1))))
+                (progn
+                  (insert ";; Tx hash: "
+                          (dapp-transfer (cdr src-cons) (cdr dst-cons) gem wad))
+                  (newline)
+                  (forward-line -1))))))
+        (forward-line 1))))))
